bd6b549a0bf7d0b0d43e252b1413e9f943d94160
Nora Schöner <nora.schoener@superluminar.io>
2023-09-25 22:15:45 +0200

    add terrafrom usecaeses

---
+++ terraform/s3-bucket/main.tf
@@ -0,0 +1,120 @@
+/* ----------------------------------------------------------
+  main.tf
+  * maintained by @norchen
+  * for educational purpose only, no production readyness
+    garantued
+------------------------------------------------------------*/
+
+/* ----------------------------------------------------------
+  set provider
+------------------------------------------------------------*/
+# the starting point to connect to AWS
+provider "aws" {
+  profile = "test"     # the profile you configured via AWS CLI
+  region  = var.region # the region you want to deploy to
+
+  # default tags to be added to every AWS ressource
+  default_tags {
+    tags = {
+      Owner = "Wolkencode"
+    }
+  }
+}
+
+# configure terraform version and backend properties
+terraform {
+  required_providers {
+
+    # sets version for AWS Terraform provider
+    # https://github.com/hashicorp/terraform-provider-aws/blob/main/CHANGELOG.md
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  # sets Terraform version
+  required_version = ">= 1.1"
+
+  # if applicable put your remote backend configuration here (e.g. S3 backend)
+  # backend "s3" {...}
+}
+
+/* ----------------------------------------------------------
+  set locals & variables
+------------------------------------------------------------*/
+locals {
+  s3_bucket_name = join("-", [
+    "my-bucket",
+    var.region,
+    formatdate("DD-MM-YY", timestamp())
+  ])
+}
+
+variable "region" {
+  description = "The AWS region to deploy to"
+  type        = string
+  default     = "us-east-1"
+}
+
+/* ----------------------------------------------------------
+  set resources
+------------------------------------------------------------*/
+# s3 bucket
+resource "aws_s3_bucket" "website" {
+  bucket = local.s3_bucket_name
+}
+
+# s3 access control lists (acl) configuration (since aws provider version 4.9)
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.website.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "website" {
+  bucket = aws_s3_bucket.website.id
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Sid" : "PublicReadGetObject",
+          "Effect" : "Allow",
+          "Principal" : "*",
+          "Action" : [
+            "s3:GetObject"
+          ],
+          "Resource" : [
+            "arn:aws:s3:::${local.s3_bucket_name}/*"
+          ]
+        }
+      ]
+    }
+  )
+}
+
+# s3 website configuration (since aws provider version 4.9)
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.website.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+# s3 object
+resource "aws_s3_object" "website" {
+  bucket       = aws_s3_bucket.website.id
+  acl          = "public-read"
+  key          = "index.html"          # how your file will be named in the S3 Bucket (we need an index.html)
+  source       = "index.html"          # set the path to your "index.html" (here it lies in the same directory)
+  content_type = "text/html"           # use the respective MIME type for your object
+  etag         = filemd5("index.html") # same path as in source
+}
+
+/* ----------------------------------------------------------
+  set output
+------------------------------------------------------------*/
+output "s3_bucket_website_url" {
+  value = aws_s3_bucket_website_configuration.website.website_endpoint
+}
---
+++ terraform/s3-bucket/terraform.tfvars
@@ -0,0 +1,9 @@
+/* ----------------------------------------------------------
+  terraform.tfvars
+  * maintained by @norchen
+  * will be read by terraform automatically if present in the same directory
+  * for educational purpose only, no production readiness guaranteed
+------------------------------------------------------------*/
+
+# add the values for used variables
+region = "us-west-1"
