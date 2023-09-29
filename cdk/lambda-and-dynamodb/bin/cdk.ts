4cfb4b6df3013731fc5a97630e6419e3cb6aae18
VerenaTraub <verenatraubmail@gmail.com>
2023-09-29 09:44:22 +0200

    fix: remove typo

--- cdk/lambda-and-dynamodb/bin/cdk.ts
+++ cdk/lambda-and-dynamodb/bin/cdk.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { LambdaDynamoDbStack } from '../lib/lambda-dynamobd-stack';
+import { LambdaDynamoDbStack } from '../lib/lambda-dynamodb-stack';
 
 const app = new cdk.App();
 
