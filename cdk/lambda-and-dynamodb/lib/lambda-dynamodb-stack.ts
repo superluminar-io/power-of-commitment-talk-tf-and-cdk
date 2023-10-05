3feac107dda3478b034234198e730ae4203ff8d9
VerenaTraub <verenatraubmail@gmail.com>
2023-10-05 09:32:53 +0200

    chore: update lambda function

--- cdk/lambda-and-dynamodb/lib/lambda-dynamodb-stack.ts
+++ cdk/lambda-and-dynamodb/lib/lambda-dynamodb-stack.ts
@@ -2,29 +2,28 @@ import { Stack, StackProps, aws_dynamodb as dynamodb, aws_lambda_nodejs as lambd
 import { Construct } from 'constructs';
 
 export class LambdaDynamoDbStack extends Stack {
-  public jugScanTable: dynamodb.Table;
 
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    this.jugScanTable = new dynamodb.Table(this, 'jug-scan-table', {
+    const jugScanTable = new dynamodb.Table(this, 'jug-scan-table', {
       partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
     });
 
     const scanTable = new lambdaNodeJs.NodejsFunction(this, 'scan-table', {
       environment: {
-        TABLE_NAME: this.jugScanTable.tableName,
+        TABLE_NAME: jugScanTable.tableName,
       },
-    });
+      });
 
     const putItem = new lambdaNodeJs.NodejsFunction(this, 'put-item', {
       environment: {
-        TABLE_NAME: this.jugScanTable.tableName,
+        TABLE_NAME: jugScanTable.tableName,
       },
     });
 
-    this.jugScanTable.grant(scanTable, 'dynamodb:Scan');
-    this.jugScanTable.grant(putItem, 'dynamodb:PutItem');
+    jugScanTable.grant(scanTable, 'dynamodb:Scan');
+    jugScanTable.grant(putItem, 'dynamodb:PutItem');
   }
 }
